2f7fdc2a9d3a2eee05bab3d94e12ea2249dd1c3a
stephaneworkspace <s.bressani@bluewin.ch>
2020-06-11 19:18:57 +0200

    add parvati remix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <hr />
+    <!--<h1>{{ msg }}</h1>
+    <hr />-->
     <h2>Psychill</h2>
     <div>
       <iframe
@@ -31,12 +31,42 @@
         >Sortna - This Is Weird (Electryps Remix)</a
       >
     </div>
-    <p>
+    <h2>Forest</h2>
+    <div>
+      <iframe
+        width="100%"
+        height="166"
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/833980315%3Fsecret_token%3Ds-4pjSQREZA3C&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      ></iframe>
+      <div
+        style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"
+      >
+        <a
+          href="https://soundcloud.com/electryps"
+          title="Electryps"
+          target="_blank"
+          style="color: #cccccc; text-decoration: none;"
+          >Electryps</a
+        >
+        ·
+        <a
+          href="https://soundcloud.com/electryps/ectogasmics-love-understanding-electryps-remix/s-4pjSQREZA3C"
+          title="Ectogasmics - Love &amp; Understanding (Electryps Remix)"
+          target="_blank"
+          style="color: #cccccc; text-decoration: none;"
+          >Ectogasmics - Love &amp; Understanding (Electryps Remix)</a
+        >
+      </div>
+    </div>
+    <!--<p>
       Check my sounds on
       <a href="https://soundcloud.com/electryps" target="_blank" rel="noopener"
         >Soundcloud</a
       >.
-    </p>
+    </p>-->
     <!--
     <h3>Installed CLI Plugins</h3>
     <ul>
