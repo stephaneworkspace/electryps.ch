159c7c76ed43cf48296b57dae415e208c8cb5ae0
stephaneworkspace <s.bressani@bluewin.ch>
2020-06-15 17:16:41 +0200

    home mastering song

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,7 +20,7 @@
         scrolling="no"
         frameborder="no"
         allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/840525067%3Fsecret_token%3Ds-JM8hgQsqJrq&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/840603730%3Fsecret_token%3Ds-IjS62PIn1rD&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
       ></iframe>
     </div>
     <h2>Forest</h2>
