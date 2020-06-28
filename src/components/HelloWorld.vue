31b45196abb5534fb04ac1132abee5723afbcbc1
stephaneworkspace <s.bressani@bluewin.ch>
2020-06-28 12:47:06 +0200

    new song

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -61,7 +61,7 @@
         scrolling="no"
         frameborder="no"
         allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/848024380%3Fsecret_token%3Ds-cNEZmJsSzSW&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/848315872%3Fsecret_token%3Ds-DJKGti8Lr6B&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
       ></iframe>
     </div>
     <!--<h2>Full On</h2>
